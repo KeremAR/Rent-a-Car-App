c7fa40ca6e4eadb0ed108e58d0478b55df034905
Kerem AR <keremar0000@gmail.com>
2024-03-09 01:19:59 +0300

    added miragejs
    mirage.js server added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import "./server"
+
 
 function App() {
   return (
