cfc0f8f89a70a8fea803354c6181ee67997c63b0
Kerem AR <keremar0000@gmail.com>
2024-03-09 13:57:49 +0300

    cars added
    cars page and cars detail page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
 import "./server"
 
 
@@ -12,11 +15,18 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+
+
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+
+
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+
+export default function Vans() {
+    const[vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+    }, [])
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+        <Link to= {`/vans/${van.id}`} 
+        aria-label={`View details for ${van.name}, 
+        priced at $${van.price} per day`}
+        >
+        <img src={van.imageUrl} alt={`Image of ${van.name}`}/>
+        <div className="van-info">
+            <p>{van.name}</p>
+            <p>${van.price}<span>/day</span></p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
+    </div>
+    ))
+
+    return (
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+        <div className="van-list">
+            {vanElements}
+        </div>
+    </div>
+
+        )
+}
