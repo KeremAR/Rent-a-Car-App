eb51a8323ce7f39574e917f639a61cd96fb2a5b4
Kerem AR <keremar0000@gmail.com>
2024-03-10 17:40:57 +0300

    added Layout
    Layout component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "./server"
 
@@ -11,22 +12,14 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-
-
-        </nav>
-      </header>
+     
       <Routes>
+        <Route element= {<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
-
-
+        </Route>
       </Routes>
     </BrowserRouter>
   )
