38a56313a2dbe27b848d4ab31d898fe9cb897eb7
Kerem AR <keremar0000@gmail.com>
2024-03-11 15:07:09 +0300

    van detail page added
    van detail page added and it's subdirectories

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import VanDetail from "./pages/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+
+
+
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
@@ -19,17 +27,24 @@ function App() {
      
       <Routes>
       <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="vans" element={<Vans />} />
         <Route path="vans/:id" element={<VanDetail />} />
        
         <Route path="host" element={<HostLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="income" element={<Income />} />
-        <Route path="reviews" element={<Reviews />} />
-        </Route>
+         <Route index element={<Dashboard />} />
+         <Route path="income" element={<Income />} />
+         <Route path="reviews" element={<Reviews />} />
+         <Route path="vans" element={<HostVans />} />
+         <Route path="vans/:id" element={<HostVanDetail />} >
+          <Route index element={<HostVanInfo />} />
+          <Route path="pricing" element={<HostVanPricing />} />
+          <Route path="photos" element={<HostVanPhotos />} />
 
+
+          </Route>
+         </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,14 +1,46 @@
 import React from "react"
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
+
 export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
         <>
-        <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/reviews">Reviews</Link>
-    </nav>
-    <Outlet />
-    </>
+            <nav className="host-nav">
+                <NavLink
+                    to="."
+                    end //ends the matching
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+
+                <NavLink
+                    to="reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
+            </nav>
+            <Outlet />
+        </>
     )
 }
