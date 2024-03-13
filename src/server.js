d6c0abd2a85479f6c7d235df9eb56e03bf7c1f99
Kerem AR <keremar0000@gmail.com>
2024-03-13 14:40:21 +0300

    css hotfix
    css class names hotfix

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ createServer({
        // this.timing= 2000
 
        this.get("/cars", (schema, request) => {
-        //return new Response(400, {}, {error: "Error fetching data"})
+       // return new Response(400, {}, {error: "Error fetching data"})
          return schema.cars.all()
     })
 
